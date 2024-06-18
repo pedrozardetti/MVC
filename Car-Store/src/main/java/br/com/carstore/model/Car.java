dc24b925ba8ce126fc83f9232f6505e77d7e7042
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-18 18:43:22 -0300

    feat: removido set e add construtor para receber os parâmetros

--- Car-Store/src/main/java/br/com/carstore/model/Car.java
+++ Car-Store/src/main/java/br/com/carstore/model/Car.java
@@ -4,12 +4,13 @@ public class Car {
 
     private String name;
 
+    public Car(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    
 
 }
