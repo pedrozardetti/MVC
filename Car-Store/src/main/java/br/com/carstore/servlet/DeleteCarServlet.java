cc0743bb382a2c5143ecd500e868279522a50468
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-18 21:32:12 -0300

    Criada classe DeleteCarServlet com end point em delete-car

--- Car-Store/src/main/java/br/com/carstore/servlet/DeleteCarServlet.java
+++ Car-Store/src/main/java/br/com/carstore/servlet/DeleteCarServlet.java
@@ -1,2 +1,23 @@
-package br.com.carstore.servlet;public class DeleteCarServlet {
+package br.com.carstore.servlet;
+
+import br.com.carstore.dao.CarDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/delete-car")
+public class DeleteCarServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String carId = req.getParameter("id");
+
+        new CarDAO().deleteCarById(carId);
+
+        resp.sendRedirect("/find-all-cars");
+    }
 }
