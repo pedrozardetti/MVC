0f899c3fb42a06507eb95a2e1597c815fc2f602a
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-20 22:57:30 -0300

    add variável dataSource e método BasicDataSource

---
+++ Car-Store/src/main/java/br/com/carstore/config/ConnectionPoolConfig.java
@@ -0,0 +1,12 @@
+package br.com.carstore.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class ConnectionPoolConfig {
+
+    private static BasicDataSource dataSource;
+
+    public static BasicDataSource getDataSource() {
+
+    }
+}
