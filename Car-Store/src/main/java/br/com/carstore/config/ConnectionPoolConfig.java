a65ac9201c5d37388b7f36d3340a4252dce059c2
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-20 23:05:42 -0300

    add parâmetros da instância BasicDataSource

--- Car-Store/src/main/java/br/com/carstore/config/ConnectionPoolConfig.java
+++ Car-Store/src/main/java/br/com/carstore/config/ConnectionPoolConfig.java
@@ -8,5 +8,20 @@ public class ConnectionPoolConfig {
 
     public static BasicDataSource getDataSource() {
 
+        if (dataSource == null) {
+            dataSource = new BasicDataSource();
+            dataSource.setUrl("jdbc:h2:~/test");
+            dataSource.setUsername("sa");
+            dataSource.setPassword("sa");
+            dataSource.setMinIdle(5);
+            dataSource.setMaxIdle(10);
+            dataSource.setMaxTotal(50);
+
+            System.out.println("New connection pool created with sucessful");
+
+
+        }
+
+        return dataSource;
     }
 }
