a9fd17c83212906d1e6488a86011e5fb4efec841
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-21 20:11:46 -0300

    add bloco try catch, preparedStatement e resultSet

--- Car-Store/src/main/java/br/com/carstore/dao/UserDao.java
+++ Car-Store/src/main/java/br/com/carstore/dao/UserDao.java
@@ -1,6 +1,12 @@
 package br.com.carstore.dao;
 
+import br.com.carstore.config.ConnectionPoolConfig;
 import br.com.carstore.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserDao {
 
@@ -8,7 +14,32 @@ public class UserDao {
 
         String SQL = "SELECT * FROM USR WHERE USERNAME = ?";
 
-        return false;
+        try {
+
+            Connection con = ConnectionPoolConfig.getConnection();
+
+            PreparedStatement preparedStatement = con.prepareStatement(SQL);
+
+            preparedStatement.setString(1, user.getUsername());
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+
+                String password = resultSet.getString("password");
+            }
+
+            System.out.println("Success in select username");
+
+            con.close();
+
+            return true;
+        } catch (Exception e) {
+
+            System.out.println("Error: " + e.getMessage());
+
+            return false;
+
+        }
 
 
     }
