5a0f582d911a0b1dbf36780cab181abe14dfffc0
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-21 20:07:14 -0300

    feat: método verifyCredentials criado e String SQL

--- Car-Store/src/main/java/br/com/carstore/dao/UserDao.java
+++ Car-Store/src/main/java/br/com/carstore/dao/UserDao.java
@@ -1,8 +1,15 @@
 package br.com.carstore.dao;
 
+import br.com.carstore.model.User;
+
 public class UserDao {
 
-    public boolean verifyCredentials() {
+    public boolean verifyCredentials(User user) {
+
+        String SQL = "SELECT * FROM USR WHERE USERNAME = ?";
+
+        return false;
+
 
     }
 }
