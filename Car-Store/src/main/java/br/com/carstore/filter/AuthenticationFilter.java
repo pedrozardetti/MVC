ffde85bd252aece16af5ba1d95cf01c22b335e03
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-21 22:35:33 -0300

    fix lógica isUserLoggedOn

--- Car-Store/src/main/java/br/com/carstore/filter/AuthenticationFilter.java
+++ Car-Store/src/main/java/br/com/carstore/filter/AuthenticationFilter.java
@@ -2,6 +2,7 @@ package br.com.carstore.filter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 // Filtro é aplicado em todas as requisições que o caminho começa com /admin/
@@ -16,13 +17,34 @@ public class AuthenticationFilter implements Filter {
 
     // Executa a lógica principal do filtro. É chamado para cada requisição que passa pelo filtro.
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
 
+        // Se o usuário estiver logado, irá repassar a requisição
+        if (isUserLoggedOn(httpServletRequest)) {
+
+            // Continua o fluxo de processamento da requisição
+            chain.doFilter(servletRequest, response);
+
+        // Se não estiver logado, ele irá para a página de login
+        } else  {
+
+            servletRequest.setAttribute("message", "User not authenticated!");
+
+            servletRequest.getRequestDispatcher("/login.jsp").forward(httpServletRequest, response);
+        }
     }
 
     // Limpa os recursos. É chamada uma vez quando o filtro está sendo removido.
     @Override
     public void destroy() {
+    }
+
+    private boolean isUserLoggedOn(HttpServletRequest httpServletRequest) {
+
+        return httpServletRequest.getSession().getAttribute("loggedUser") != null;
 
     }
+
 }
+
