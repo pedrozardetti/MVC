80ff209fe7317aa311a5ee8cf8a0b46f1afc3d3b
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-24 17:14:33 -0300

    fix getter e setter

--- Spotify/ProjetoSpotify/src/main/java/br/com/projetospotify/model/Usuario.java
+++ Spotify/ProjetoSpotify/src/main/java/br/com/projetospotify/model/Usuario.java
@@ -1,15 +1,40 @@
 package br.com.projetospotify.model;
 
-public class Usuario implements java.io.Serializable {
+public class Usuario {
     private int id;
     private String username;
     private String fullname;
     private String email;
 
-    public Usuario(int id, String username, String fullname, String email) {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
         this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(java.lang.String fullname) {
         this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
 }
