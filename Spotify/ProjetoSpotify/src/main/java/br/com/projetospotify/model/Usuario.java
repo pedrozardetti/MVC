d44350d4167fed5936afceda35fa015096deac07
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-24 16:58:39 -0300

    add varíaveis e construtor

--- Spotify/ProjetoSpotify/src/main/java/br/com/projetospotify/model/Usuario.java
+++ Spotify/ProjetoSpotify/src/main/java/br/com/projetospotify/model/Usuario.java
@@ -1,4 +1,15 @@
 package br.com.projetospotify.model;
 
-public class Usuario {
+public class Usuario implements java.io.Serializable {
+    private int id;
+    private String username;
+    private String fullname;
+    private String email;
+
+    public Usuario(int id, String username, String fullname, String email) {
+        this.id = id;
+        this.username = username;
+        this.fullname = fullname;
+        this.email = email;
+    }
 }
