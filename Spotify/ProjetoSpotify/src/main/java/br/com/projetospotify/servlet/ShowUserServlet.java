fdadd63d0e640b7ccfd703f42c5a6e7232f5788d
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-24 17:39:15 -0300

    feat: método doGet adicionado

--- Spotify/ProjetoSpotify/src/main/java/br/com/projetospotify/servlet/ShowUserServlet.java
+++ Spotify/ProjetoSpotify/src/main/java/br/com/projetospotify/servlet/ShowUserServlet.java
@@ -1,6 +1,15 @@
 package br.com.projetospotify.servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class ShowUserServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("ok.jsp");
+    }
 }
