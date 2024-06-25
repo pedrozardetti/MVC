3bb6c134eca31e139ed92c2b43c5b43619fb6ea1
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-06-25 14:39:37 -0300

    Adicionado ShowUserServlet para encaminhar GET a ok.jsp, mapeando /show-user

--- Spotify/ProjetoSpotify/src/main/java/br/com/projetospotify/servlet/ShowUserServlet.java
+++ Spotify/ProjetoSpotify/src/main/java/br/com/projetospotify/servlet/ShowUserServlet.java
@@ -1,15 +1,24 @@
 package br.com.projetospotify.servlet;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/show-user")
 public class ShowUserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.sendRedirect("ok.jsp");
+        RequestDispatcher dispatcher;
+        dispatcher = getServletContext().getRequestDispatcher("/ok.jsp");
+
+        System.out.println("Chegou aqui");
+
+        dispatcher.forward(req, resp);
+
     }
 }
